e4077542b0368a519815999132e2d1afb78cfa84
Ugonna Okoli <ugokoli30@gmail.com>
2020-03-20 15:01:14 +0100

    Added full implementation for FingerSelector

--- app/src/main/java/com/ugokoli/styroview/samples/FingerSelectorSample.kt
+++ app/src/main/java/com/ugokoli/styroview/samples/FingerSelectorSample.kt
@@ -1,19 +1,48 @@
 package com.ugokoli.styroview.samples
 
 import android.os.Bundle
+import android.widget.RadioGroup
 import com.ugokoli.styroview.FingerSelector
 import com.ugokoli.styroview.R
 import com.ugokoli.styroview.constants.Finger
 import com.ugokoli.styroview.constants.Hand
 import kotlinx.android.synthetic.main.activity_finger_selector_sample.*
 
-class FingerSelectorSample : BaseSample(), FingerSelector.FingerSelectedListener {
+class FingerSelectorSample : BaseSample(), RadioGroup.OnCheckedChangeListener, FingerSelector.FingerSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finger_selector_sample)
+
+        radioFinger.setOnCheckedChangeListener(this)
+        fingerSelector.fingerSelectedListener = this
+
+        onFingerSelected(fingerSelector.hand, fingerSelector.finger)
+    }
+
+    override fun onCheckedChanged(radioGroup: RadioGroup?, i: Int) {
+        if (i == R.id.radioLeftThumb) {
+            fingerSelector.hand = Hand.LEFT
+        } else if (i == R.id.radioRightThumb) {
+            fingerSelector.hand = Hand.RIGHT
+        }
     }
 
     override fun onFingerSelected(hand: Hand, finger: Finger) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        selectedTxt.setText(String.format(
+                getString(R.string.select_finger),
+                hand.toString(),
+                finger.toString()
+        ))
+
+        when (hand) {
+            Hand.LEFT -> {
+                radioFinger.check(R.id.radioLeftThumb)
+            }
+            Hand.RIGHT -> {
+                radioFinger.check(R.id.radioRightThumb)
+            }
+            else -> {
+            }
+        }
     }
 }
